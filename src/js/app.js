3716c584ffa365a48ac4dc780ba33470cb0e643f
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-02 16:23:01 +0200

    add SASS

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from "react-router-dom";
+import Supervisor from './Components/Supervisor'
 
 import './../sass/style.scss'; // adres do głównego pliku SASS
 
-const urlUsers = 'http://localhost:3000/users'
-const urlSchedules = 'http://localhost:3000/schedules'
-const urlScheduleUser = 'http://localhost:3000/scheduleUser'
-
-const getData = (url) => {
-    fetch(url).then(resp => {
-        const data = resp.text()
-        return data
-    }).then(data => {
-        console.log(data)
-    }).catch(err => (
-        console.log(err)
-    ))
+class App extends Component {
+    render() {
+        return (
+            <>
+                <HashRouter>
+                    <Switch>
+                        <Route exact path='/' component={Supervisor} />
+                    </Switch>
+                </HashRouter>
+            </>
+        )
+    }
 }
-
-getData(urlScheduleUser)
-
-const App = () => (
-    <h1>sss</h1>
-)
 
 ReactDOM.render(
     <App />,
