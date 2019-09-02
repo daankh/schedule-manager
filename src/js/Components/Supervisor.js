b6d1b686d584bcca6713f37e94cb33244d48d804
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-02 22:29:35 +0200

    add supervisor menu

--- src/js/Components/Supervisor.js
+++ src/js/Components/Supervisor.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from "react-router-dom";
 
 class Supervisor extends Component {
     constructor(props) {
@@ -8,10 +15,40 @@ class Supervisor extends Component {
 
     render() {
 
+        const active = {
+            backgroundColor: "#FD7272",
+        }
+
         return (
-            <div className="wrapper">
-                <h1>Supervisor</h1>
+            <div className="supervisor" >
+                <div className="wrapper">
+                    <HashRouter>
+                        <nav className="navigation">
+                            <ul>
+                                <li>
+                                    <NavLink className="link" activeStyle={active} exact to="/supervisor/mySchedule">Mój grafik</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="link" activeStyle={active} to="/supervisor/employersManaging">Zarządanie pracownikami</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="link" activeStyle={active} to="/supervisor/addSchedule">Dodaj grafik</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="link" activeStyle={active} to="/supervisor/editSchedules">Edycja grafików</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="link" to="/">Wyloguj się</NavLink>
+                                </li>
+                            </ul>
+                        </nav>
+                        <Switch>
+
+                        </Switch>
+                    </HashRouter>
+                </div>
             </div>
+
         )
     }
 }
