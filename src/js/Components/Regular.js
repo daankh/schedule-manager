0c5929866c2e420b5237a5ba6f23426a6875e13f
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-02 22:48:02 +0200

    add user menu

--- src/js/Components/Regular.js
+++ src/js/Components/Regular.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from "react-router-dom";
 
 class Regular extends Component {
 
     render() {
 
+        const active = {
+            backgroundColor: "#FD7272",
+        }
+
         return (
-            <div className="wrapper">
-                <h1>Regular emp view</h1>
+            <div className="regular" >
+                <div className="wrapper">
+                    <HashRouter>
+                        <nav className="navigation">
+                            <ul>
+                                <li>
+                                    <NavLink className="link" activeStyle={active} exact to="/regular/mySchedule">Mój grafik</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="link" activeStyle={active} to="/regular/requests">Prośby</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="link" activeStyle={active} to="/regular/settings">Ustawienia</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="link" to="/">Wyloguj się</NavLink>
+                                </li>
+                            </ul>
+                        </nav>
+                        <Switch>
+
+                        </Switch>
+                    </HashRouter>
+                </div>
             </div>
+
         )
     }
 }
