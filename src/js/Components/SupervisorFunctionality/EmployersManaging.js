6cc1fd940d87adbf7c1e8a5651488a8f85e3dec6
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-04 07:42:26 +0200

    add files

--- src/js/Components/SupervisorFunctionality/EmployersManaging.js
+++ src/js/Components/SupervisorFunctionality/EmployersManaging.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 
 class employersManaging extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
 
     render() {
 
         return (
-            <h1>employersManaging</h1>
+            <div className="employersManaging">
+                <div className="wrapper">
+                    <div className="addEmployer">
+                        <h2>Dodaj pracownika</h2>
+                        <form>
+                            <label>
+                                <span>Imię</span>
+                                <input type="text" />
+                            </label>
+                            <label>
+                                <span>Nazwisko</span>
+                                <input type="text" />
+                            </label>
+                            <label>
+                                <span>Mail</span>
+                                <input type="email" />
+                            </label>
+                            <label>
+                                <span>Wymiar etatu</span>
+                                <select>
+
+                                </select>
+                            </label>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
