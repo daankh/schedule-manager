ef3031f613d5fe380b42d773ac60cf2e1bdd0b48
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-05 00:32:04 +0200

    add functionality - adding new schedule

---
+++ src/js/Components/SupervisorFunctionality/AddScheduleForm.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import moment from 'moment'
+
+class AddScheduleForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            months: [{ number: 0, name: 'Styczeń' }, { number: 1, name: 'Luty' }, { number: 2, name: 'Marzec' }, { number: 3, name: 'Kwiecień' }, { number: 4, name: 'Maj' }, { number: 5, name: 'Czerwiec' }, { number: 6, name: 'Lipiec' }, { number: 7, name: 'Sierpień' }, { number: 8, name: 'Wrzesień' }, { number: 9, name: 'Październik' }, { number: 10, name: 'Listopad' }, { number: 11, name: 'Grudzień' }],
+            years: [moment().get('year'),
+            moment().add(1, 'years').get('year'),
+            moment().add(2, 'years').get('year')],
+
+            selectedMonth: this.props.month,
+            selectedYear: this.props.year,
+        }
+    }
+
+    updateValue = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    addSchedule = (e) => {
+        e.preventDefault()
+
+        const startDay = moment({
+            year: this.state.selectedYear,
+            month: this.state.selectedMonth,
+            day: 1
+        })
+
+        const endDay = moment({
+            year: this.state.selectedYear,
+            month: this.state.selectedMonth,
+            day: 1
+        }).add(1, 'months').subtract(1, 'days')
+
+        const daysNumber = endDay.get('date');
+
+        const days = []
+
+        for (let i = 1; i <= daysNumber; i++) {
+            days.push({
+                day: i,
+                employers: {
+                    from7to15: [],
+                    from7to19: [],
+                    from19to7: [],
+                }
+            })
+        }
+
+        const schedule = {
+            month: this.state.selectedMonth,
+            year: this.state.selectedYear,
+            day: days
+        }
+
+        fetch(this.props.urlSchedules, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(schedule)
+        }).then(data => {
+            console.log('Pomyślnie dodano harmonogram')
+            this.props.updateSchedules()
+        }).catch(err => console.log(err, 'nie dodano harmonogramu '))
+    }
+
+
+    render() {
+
+        const monthsSelection = this.state.months.map(month => (
+            <option key={month.number} value={month.number}>{month.name}</option>
+        ))
+        const yearsSelection = this.state.years.map(year => (
+            <option key={year} value={year}>{year}</option>
+        ))
+
+        return (
+            <form onSubmit={this.addSchedule}>
+                <span>
+                    <span>Nowy harmonogram pracy na miesiąc</span>
+                    <select name="selectedMonth" value={this.state.selectedMonth} onChange={this.updateValue}>
+                        {monthsSelection}
+                    </select>
+                    <select name="selectedYear" value={this.state.selectedYear} onChange={this.updateValue}>
+                        {yearsSelection}
+                    </select>
+                    <span>roku</span>
+                </span>
+                <input type="submit" value="Dodaj" />
+            </form>
+        )
+    }
+}
+
+export default AddScheduleForm
--- src/js/Components/SupervisorFunctionality/Schedules.js
+++ src/js/Components/SupervisorFunctionality/Schedules.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import moment from 'moment'
+import AddScheduleForm from './AddScheduleForm'
+import SchedulesList from './SchedulesList'
 
 class Schedules extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            schedules: [],
+            urlSchedules: 'http://localhost:3000/schedules',
+
+            selectedMonth: moment().add(1, 'months').get('month'),
+            selectedYear: moment().add(1, 'month').get('year')
+        }
+    }
+
+    updateValue = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    updateSchedulesHandler = () => {
+        fetch(this.state.urlSchedules, {
+            method: 'GET'
+        }).then(resp => {
+            if (resp.ok)
+                return resp.json();
+            else
+                throw new Error('Błąd sieci!');
+        }).then(data => {
+            this.setState({
+                schedules: data,
+            })
+        }).catch(err => (
+            console.log(err)
+        ))
+    }
+
+    componentDidMount() {
+        this.updateSchedulesHandler()
+    }
 
     render() {
 
         return (
-            <h1>Schedules</h1>
+            <div className="schedules">
+                <div className="wrapper">
+                    <AddScheduleForm urlSchedules={this.state.urlSchedules} updateSchedules={this.updateSchedulesHandler} month={this.state.selectedMonth} year={this.state.selectedYear} />
+                    <h2>Harmonogramy</h2>
+                    <SchedulesList schedules={this.state.schedules} />
+                </div>
+            </div>
         )
     }
 }
