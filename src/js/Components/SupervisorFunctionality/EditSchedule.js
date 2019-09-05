6ea81e797c23abf28bf118d0e53b335839aaf969
Daniel Jarosz <jarosz.dan@gmail.com>
2019-09-05 12:19:36 +0200

    add functionality - removing schedule

---
+++ src/js/Components/SupervisorFunctionality/EditSchedule.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class EditSchedule extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            schedules: this.props.schedules,
+            scheduleUsers: this.props.scheduleUsers,
+            users: this.props.users,
+
+            schedule: null,
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+        console.log(this.state.scheduleUsers)
+
+        return (
+            <div className="editSchedule">
+                <div className="wrapper">
+                    <div className="row editSchedule__header">
+                        <h2>Edycja grafiku za miesiąc ...</h2>
+                        <button className="delete">Odrzuć zmiany</button>
+                        <button className="save">Zapisz</button>
+                    </div>
+                    <div className="row">
+
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default EditSchedule
